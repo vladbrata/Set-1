122411cdc952400058a57c70984a68dafb5ea3d8
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-05 18:13:23 +0200

    Final Touches
    Improvements to the code

--- #5/Program.cs
+++ #5/Program.cs
@@ -1,28 +1,28 @@
 ﻿// Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga.
-
-int num = 1467;
-int[] arr = NumToArr(num);
+Random random = new Random();
+int num = random.Next(1, 1000);
+Console.WriteLine($"Se da numarul {num}");
 
 Console.Write("Introdu un numar k: ");
 int k = Convert.ToInt32(Console.ReadLine());
 
-PrintResult(num, k);
-
+Console.WriteLine(PrintResult(num, k));
 
 static string PrintResult(int num, int k)
 {
+    int[] arr = NumToArr(num);
     if (k > Digits(num)) {
         return "Numarul este prea scurt!";
     }
     else {
-        return $"A {k} cifra din numarul {num} este {arr[k]}";
+        return $"A {k} cifra din numarul (de la dreapta la stanga) {num} este {arr[k - 1]}";
     }
 }
 
 static int[] NumToArr(int num)
 {
     int[] arr = new int[Digits(num)];
-    for (int i = arr.Length - 1; i >= 0; i--) {
+    for (int i = 0; i < arr.Length; i++) {
         arr[i] = num % 10;
         num = num / 10;
     }

69176f8e21c80fc8013ed5d5641eb6636ea082e3
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-05 16:23:00 +0200

    Rezolvare Exercitiu
    Rezolvare

---
+++ #3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Determinati daca n se divide cu k, unde n si k sunt date de intrare.
+Console.Write("n:");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.Write("k:");
+double k = Convert.ToDouble(Console.ReadLine());
+double calcul = k / n;
+
+if (calcul % 1 == 0)
+{
+    Console.WriteLine("n se divide cu k");
+}
+else 
+{
+    Console.WriteLine("n nu se divide cu k");
+}

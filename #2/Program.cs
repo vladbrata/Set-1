f4e8b688bd6c00c82dd64ba6d0498aae4552b83d
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-05 15:04:12 +0200

    rezolvare
    d

--- #2/Program.cs
+++ #2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Result(a, b, c);
+
+static void Result(int a, int b, int c)
+{
+    double delta = Math.Pow(b, 2) - 4 * a * c;
+    double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+    double x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+    if (x1 % 1 == 0 && x2 % 1 == 0)
+    {
+        Console.WriteLine($"Se rezolva ecuatia: {a}x^2 + {b}x + {c} = 0\n");
+        Console.WriteLine($"Delta = b^2 - 4 * a * c");
+        Console.WriteLine($"Delta = {b}^2 - 4 * {a} * {c}");
+        Console.WriteLine($"Delta = {delta}\n");
+        Console.WriteLine($"x1 = (-b + √delta) / 2 * a");
+        Console.WriteLine($"x1 = (-{b} + √{delta}) / 2 * {a}");
+        Console.WriteLine($"x1 = {x1}\n");
+        Console.WriteLine($"x2 = (-b - √delta) / 2 * a");
+        Console.WriteLine($"x1 = (-{b} - √{delta}) / 2 * {a}");
+        Console.WriteLine($"x1 = {x2}\n");
+        Console.WriteLine($"x1: {x1}\tx2: {x2}");
+    }
+    else
+    {
+        Console.WriteLine("Problema nu are solutii reale!");
+    }
+}

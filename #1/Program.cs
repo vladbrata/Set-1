f79a7f5be4f7bb75b1648ab6f0f4bb80d51af6b1
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-05 11:08:07 +0200

    improvement
    improvement

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,19 +1,16 @@
 ﻿
-//Date de intrare
 Console.Write("a:");
- double a = Convert.ToDouble(Console.ReadLine());
- Console.Write("b:");
- double b = Convert.ToDouble(Console.ReadLine());
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b:");
+double b = Convert.ToDouble(Console.ReadLine());
 
- Calcul1(a, b);
+Console.WriteLine($"Se rezolva ecuatia {a}x + {b} = 0");
+Console.WriteLine($"x = -b / a");
+Console.WriteLine($"x = -{b} / {a}");
+Console.WriteLine("x = " + Result(a, b));
 
-static void Calcul1(double a, double b)
+static double Result(double a, double b)
 {
-    // Formula de calcul
     double x = -b / a;
-    // Rezolvare
-    Console.WriteLine($"Se rezolva ecuatia {a}x + {b} = 0");
-    Console.WriteLine($"x = -b / a");
-    Console.WriteLine($"x = -{b} / {a}");
-    Console.WriteLine($"x: {x}");
+    return x;
 }

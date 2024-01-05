9a4e1b4612dfd3e44e3e2c920d6e81d0e476cff8
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-05 16:28:45 +0200

    Rezolvare
    Rezolvare Exercitiu

--- #4/Program.cs
+++ #4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Detreminati daca un an y este an bisect.
+Console.WriteLine("Cate zile are anul?");
+Console.WriteLine("Daca are 365 de zile, apasati 1, iar daca are 366, apasati 2");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (y == 1)
+{
+    Console.WriteLine("Anul nu este bisect!");
+}
+else {
+    Console.WriteLine("Anul este bisect!");
+}    

c13152a07bb645eff42d27dd719fa9bb76719ad0
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-05 11:00:20 +0200

    Prima Problema
    Rezolvarea la prima problema

---
+++ #1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Date de intrare
+Console.Write("a:");
+ double a = Convert.ToDouble(Console.ReadLine());
+ Console.Write("b:");
+ double b = Convert.ToDouble(Console.ReadLine());
+
+ Calcul1(a, b);
+
+static void Calcul1(double a, double b)
+{
+    // Formula de calcul
+    double x = -b / a;
+    // Rezolvare
+    Console.WriteLine($"Se rezolva ecuatia {a}x + {b} = 0");
+    Console.WriteLine($"x = -b / a");
+    Console.WriteLine($"x = -{b} / {a}");
+    Console.WriteLine($"x: {x}");
+}

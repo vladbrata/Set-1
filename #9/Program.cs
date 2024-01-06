fd981f30e6763b84638ba7c7acb0cd79579c70e5
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 11:07:34 +0200

    Rezolvare
    Rezolvare Exercitiu #9

--- #9/Program.cs
+++ #9/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Afisati toti divizorii numarului n.
+Console.Write("Da-mi un numar n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nDivizorii numarului {n} sunt: ");
+GetDivizori(n);
+
+static void GetDivizori(int n)
+{
+    for (int i = 1; i <= n; i++) {
+       if (n == 0) {
+           throw new ArgumentException("Invalid!");
+       }
+       else if (n == 1) {
+           throw new ArgumentException("Invalid");
+       }
+       if (n % i == 0) {
+            Console.Write(i + " ");
+        }
+    }
+}

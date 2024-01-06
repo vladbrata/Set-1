4b5420f96767662134534c753fa5b184523e3360
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 10:30:54 +0200

    Rezolvare
    Rezolvarea exercitiului

--- #7/Program.cs
+++ #7/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// (Swap) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. 
+Console.WriteLine("Introdu doua numere");
+
+Console.Write("a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+SwapNumbers(ref a, ref b);
+Console.WriteLine("Numerele dupa inversare");
+Console.WriteLine($"a: {a}");
+Console.WriteLine($"b: {b}");
+
+static void SwapNumbers(ref int a, ref int b)
+{    int swap1 = a;
+    a = b;
+    b = swap1;
+}

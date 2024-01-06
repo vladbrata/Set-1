d39347dc5d907edef48b98eadc464ac7a9a5decb
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 10:56:08 +0200

    Rezolvare
    Rezolvare exercitiu #8

--- #8/Program.cs
+++ #8/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// (Swap restrictionat) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare
+
+Console.WriteLine("Introdu doua numere");
+
+Console.Write("a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+SwapNums(a, b);
+Console.WriteLine("Numerele dupa inversare");
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+
+static void SwapNums(int a, int b)
+{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}

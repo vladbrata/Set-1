b3c14ce6109341f278bf30205f89edbfd691a98f
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 10:18:58 +0200

    Exercitiul #6
    Rezolvarea Exercitiului #6

--- #6/Program.cs
+++ #6/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi.
+Console.Write("a:");
+int a = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (IsTriangle(a, b, c)) {
+    Console.WriteLine($"{a}, {b}, {c} pot fi laturile unui triunghi");
+}
+else {
+    Console.WriteLine($"{a}, {b}, {c} nu pot fi laturile unui triunghi");
+
+}
+
+static bool IsTriangle(int a, int b, int c)
+{
+    if (a + b > c || a + c > b || c + b > a)
+    {
+        return true;
+    }
+    return false;
+}

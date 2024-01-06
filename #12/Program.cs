859001ee3f9aeaa6a15dca3bb999d55694cda354
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 14:22:20 +0200

    Rezolvare
    Rezolvare Exercitiul #12

--- #12/Program.cs
+++ #12/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Determinati cate numere integi divizibile cu n se afla in intervalul [a, b]. 
+Console.Write("Da-mi un numar a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nDa-mi un numar b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nDa-mi un numar n: ");
+int n = 2;
+
+
+int cnt = 0;
+for (int numar = a; numar <= b; numar++) {
+    if (numar % n == 0) cnt++;
+}
+
+Console.WriteLine($"\nNumarul de numere divizibile cu {n} din intervalul [{a}, {b}] sunt {cnt}");

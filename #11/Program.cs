51160938ec0a71dafb9264078002bd553178a8d7
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 14:12:58 +0200

    Rezolvare
    Rezolvarea exercitiului #11

--- #11/Program.cs
+++ #11/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Afisati in ordine inversa cifrele unui numar n. 
+Console.Write("Da-mi un numar n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int reverseInt = ReverseInt(n);
+
+Console.Write($"\nNumarul inversat este: {reverseInt}");
+
+static int ReverseInt(int num)
+{
+    int result = 0;
+    while (num > 0) {
+        result = result * 10 + num % 10;
+        num /= 10;
+    }
+    return result;
+}

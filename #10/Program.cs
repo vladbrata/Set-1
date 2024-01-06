1ec2165c92d97973b18925c5996cf6efb0cda055
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-06 11:16:46 +0200

    Rezolvare
    Rezolvarea exercitiului #10

--- #10/Program.cs
+++ #10/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Test de primalitate: determinati daca un numar n este prim.
+
+Console.Write("Introdu un numar n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintIsPrime(n));
+
+static bool IsPrime(int n)
+{
+    if (n < 2) {
+        return false;
+    }
+    else {
+        for (int d = 2; d * d <= n; d++)
+        {
+            if (n % d == 0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+static string PrintIsPrime(int n)
+{
+    bool isPrime = IsPrime(n);
+
+    if (!isPrime) {
+        return $"{n} nu este numar prim";
+    }
+    
+    return $"{n} este numar prim";
+}
